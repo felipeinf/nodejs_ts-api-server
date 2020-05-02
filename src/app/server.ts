febac2737e363ea123535397fb6e0b7665ff3419
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-01 20:59:21 -0400

    initial commit

---
+++ src/app/server.ts
@@ -0,0 +1,4 @@
+function main(){
+  console.log('Hello world');
+}
+export default { main };
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Typescript API Template
+ * 
+ * @description API REST template developed with Node.js, Typescript and Express. 
+ * @author Felipe Concha Almeida
+ * @version 1.0
+ * @license Apache
+ */
+
+import server from "app/server";
+
+server.main();
