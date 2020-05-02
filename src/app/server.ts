6dfb7600e54d145988e1197b8c6568bcda07f777
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-02 00:04:00 -0400

    feat(app): use morgan

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,10 +1,13 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 
 function main(){
   const server: Express = express();
   const port: number = parseInt(process.env.TS_API_TEMPLATE_PORT || '3000');
 
   server.use(express.json());
+  server.use(morgan('dev'));
+
   server.listen(port, () => console.log(`Server listening on port ${port}`));
 }
 export default { main };
