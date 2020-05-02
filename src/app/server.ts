7a9031496aa0c59fd5d730233ecbe7c75e72c46f
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-02 00:02:58 -0400

    feat(app): server listening

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,4 +1,10 @@
+import express, { Express } from "express";
+
 function main(){
-  console.log('Hello world');
+  const server: Express = express();
+  const port: number = parseInt(process.env.TS_API_TEMPLATE_PORT || '3000');
+
+  server.use(express.json());
+  server.listen(port, () => console.log(`Server listening on port ${port}`));
 }
 export default { main };
