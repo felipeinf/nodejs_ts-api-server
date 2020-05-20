e9066662a226924ade54af2227a178315e6bd2b6
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-19 23:43:20 -0400

    feat(user): get user by id EP in network layer

--- src/app/components/user/user.network.ts
+++ src/app/components/user/user.network.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from "express";
 import { User } from "./user.model";
 import response from "app/modules/network/response.module";
 import controller from "./user.controller";
+import user from "./user.component";
 
 
 const router: Router = express.Router();
@@ -14,6 +15,19 @@ router.get('/all', async (req: Request, res: Response) => {
   try {
     const result: Array<User> = await controller.getUsers();
     response.success(req, res, result);
+  }
+  catch (error) {
+    console.error(error);
+    response.error(req, res, 'Invalid information', 500);
+  }
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+
+  try {
+    const result: Array<User> = await controller.getUser(id);
+    response.success(req, res, result.shift());
   }
   catch (error) {
     console.error(error);
