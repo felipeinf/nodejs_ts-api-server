b13b7f49dcb6c5fe64d9f17bd6fd00f99c44e505
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-05-19 23:04:51 -0400

    feat(user): get all EP in network layer

--- src/app/components/user/user.network.ts
+++ src/app/components/user/user.network.ts
@@ -1,11 +1,24 @@
 import express, { Request, Response, Router } from "express";
+import { User } from "./user.model";
 import response from "app/modules/network/response.module";
+import controller from "./user.controller";
 
 
 const router: Router = express.Router();
 
 router.get('', (req: Request, res: Response) => {
-  response.success(req, res, 'User work!');
-})
+  response.success(req, res, 'User work!')
+});
+
+router.get('/all', async (req: Request, res: Response) => {
+  try {
+    const result: Array<User> = await controller.getUsers();
+    response.success(req, res, result);
+  }
+  catch (error) {
+    console.error(error);
+    response.error(req, res, 'Invalid information', 500);
+  }
+});
 
 export default router;
